b6d7e9c09a152ed56f8b778fa23d47672fe91b09
Jayant Goel <jgoel92@gmail.com>
2022-02-22 15:23:16 +0530

    Initial Commit

---
+++ Tutorial 2/Q4.R
@@ -0,0 +1,15 @@
+# a.
+
+i <- 10 : 100
+v <- sum((i ^ 3) + 4 * (i ^ 2))
+
+print("a.")
+print(v)
+
+# b.
+
+i <- 1.0 : 25.0
+v <- sum((2 ^ i)/i + (3 ^ i)/(i * i))
+
+print("b.")
+print(v)
---
+++ Tutorial 2/Q5.R
@@ -0,0 +1,11 @@
+# Creating Factors in R
+
+a <- c(1, -5, 3.4, -2, 5, 3)
+
+print(factor(a))
+
+print(factor(a, 1:5))
+
+print(factor(a, 3:5))
+
+print(factor(a, -10:5))
