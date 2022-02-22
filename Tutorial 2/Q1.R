97585f811c2b898666955b71e0100351c4426294
Jayant Goel <jgoel92@gmail.com>
2022-02-22 12:14:25 +0530

    Initial Commit

---
+++ Tutorial 2/Q1.R
@@ -0,0 +1,26 @@
+# a.
+v <- 1 : 20
+print(v)
+
+# b.
+v <- 20 : 1
+print(v)
+
+# c.
+v <- c(1 : 20,19 : 1)
+print(v)
+
+# d.
+tmp <- c(4,6,3)
+print(tmp)
+
+# e.
+v <- rep(tmp,10)
+print(v)
+
+# f.
+v <- c(v,4)
+print(v)
+
+v <- rep(tmp,l=31)
+print(v)
