1ebdc11f260222099fa1281185ac6e9521620721
Jayant Goel <jgoel92@gmail.com>
2022-03-11 14:55:15 +0530

    Initial Commit

--- Lecture 4/1.R
+++ Lecture 4/1.R
@@ -12,3 +12,19 @@ l <- list(name1 = c("A","B","C"), name2 = c("X", "Y", "Z"))
 
 print(l)
 print(l['name1'])
+
+# Merging List
+
+x1 <- list(20, 30, 40)
+x2 <- list("A", "B", "C")
+
+x <- c(x1, x2)
+print(x)
+
+# Converting List to Vector
+
+x1 <- unlist(x1)
+x2 <- unlist(x2)
+
+print(x1)
+print(x2)
